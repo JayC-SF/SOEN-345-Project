7d4e58ac6fd9f01da0c81e6c1f53b34f0dd79eab
Juan-Carlos Sreng-Flores <srengfloresj@hotmail.com>
2025-04-09 03:58:30 -0400

    removing jackson because not working for pitest and jacoco

--- JacksonDatabind/jackson-databind/src/test-jdk17/java/com/fasterxml/jackson/databind/typepollution/TypePollutionSuite.java
+++
@@ -1,9 +0,0 @@
-package com.fasterxml.jackson.databind.typepollution;
-
-import org.junit.platform.suite.api.SelectPackages;
-import org.junit.platform.suite.api.Suite;
-
-@Suite
-@SelectPackages("com.fasterxml.jackson.databind.typepollution")
-public class TypePollutionSuite {
-}
